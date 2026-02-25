a07547e701801e039405e3ed467940b5d255a179
MATEUS SIMOES PEREIRA <mateus.spereira8@senacsp.edu.br>
2026-02-25 17:23:04 -0300

    Estoque

---
+++ Verificação de Estoque/Verificação de Estoque/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Verificação_de_Estoque
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTexto = new System.Windows.Forms.Label();
+            this.txtResposta = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTexto
+            // 
+            this.lblTexto.AutoSize = true;
+            this.lblTexto.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTexto.Location = new System.Drawing.Point(195, 114);
+            this.lblTexto.Name = "lblTexto";
+            this.lblTexto.Size = new System.Drawing.Size(377, 25);
+            this.lblTexto.TabIndex = 0;
+            this.lblTexto.Text = "Quanto unidades tem no estoque?";
+            // 
+            // txtResposta
+            // 
+            this.txtResposta.Font = new System.Drawing.Font("Century", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResposta.Location = new System.Drawing.Point(200, 164);
+            this.txtResposta.Name = "txtResposta";
+            this.txtResposta.Size = new System.Drawing.Size(372, 33);
+            this.txtResposta.TabIndex = 1;
+            this.txtResposta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Cursor = System.Windows.Forms.Cursors.PanNorth;
+            this.btnEnviar.Location = new System.Drawing.Point(343, 226);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = false;
+            this.btnEnviar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnEnviar_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtResposta);
+            this.Controls.Add(this.lblTexto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTexto;
+        private System.Windows.Forms.TextBox txtResposta;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ Verificação de Estoque/Verificação de Estoque/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Verificação_de_Estoque
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_MouseClick(object sender, MouseEventArgs e)
+        {
+            var quantidade = Convert.ToInt32(txtResposta.Text);
+
+            if (quantidade < 5)
+            {
+                MessageBox.Show("Alerta: Baixo estoque. Por favor, reabasteça este produto.");
+            }
+            else 
+            {
+                MessageBox.Show("Estoque suficiente!");
+            }
+
+
+        }
+    }
+}
