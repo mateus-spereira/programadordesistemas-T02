998eb8511ddd35bd14af3ca41b440be65fdcdfe2
MATEUS SIMOES PEREIRA <mateus.spereira8@senacsp.edu.br>
2026-03-09 16:51:33 -0300

    Login

--- Login/Login/Form1.Designer.cs
+++ Login/Login/Form1.Designer.cs
@@ -28,20 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textUser = new System.Windows.Forms.TextBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
             this.btnEntrar = new System.Windows.Forms.Button();
-            this.textPass = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.lblLogin = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textUser
+            // txtUser
             // 
-            this.textUser.Location = new System.Drawing.Point(276, 146);
-            this.textUser.Name = "textUser";
-            this.textUser.Size = new System.Drawing.Size(248, 20);
-            this.textUser.TabIndex = 0;
+            this.txtUser.Location = new System.Drawing.Point(276, 146);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(248, 20);
+            this.txtUser.TabIndex = 0;
             // 
             // btnEntrar
             // 
@@ -55,12 +55,12 @@
             this.btnEntrar.UseVisualStyleBackColor = true;
             this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
             // 
-            // textPass
+            // txtPass
             // 
-            this.textPass.Location = new System.Drawing.Point(276, 205);
-            this.textPass.Name = "textPass";
-            this.textPass.Size = new System.Drawing.Size(248, 20);
-            this.textPass.TabIndex = 2;
+            this.txtPass.Location = new System.Drawing.Point(276, 205);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(248, 20);
+            this.txtPass.TabIndex = 2;
             // 
             // panel1
             // 
@@ -79,9 +79,9 @@
             this.lblLogin.Font = new System.Drawing.Font("Microsoft YaHei", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblLogin.Location = new System.Drawing.Point(342, 19);
             this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(114, 42);
+            this.lblLogin.Size = new System.Drawing.Size(108, 42);
             this.lblLogin.TabIndex = 1;
-            this.lblLogin.Text = "label2";
+            this.lblLogin.Text = "Login";
             this.lblLogin.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Form1
@@ -91,9 +91,9 @@
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.textPass);
+            this.Controls.Add(this.txtPass);
             this.Controls.Add(this.btnEntrar);
-            this.Controls.Add(this.textUser);
+            this.Controls.Add(this.txtUser);
             this.ForeColor = System.Drawing.Color.LightSkyBlue;
             this.Name = "Form1";
             this.Text = "Login";
@@ -106,9 +106,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textUser;
+        private System.Windows.Forms.TextBox txtUser;
         private System.Windows.Forms.Button btnEntrar;
-        private System.Windows.Forms.TextBox textPass;
+        private System.Windows.Forms.TextBox txtPass;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label lblLogin;
     }
--- Login/Login/Form1.cs
+++ Login/Login/Form1.cs
@@ -12,6 +12,7 @@ namespace Login
 {
     public partial class Form1 : Form
     {
+        int tentativas = 0;
         public Form1()
         {
             InitializeComponent();
@@ -22,18 +23,29 @@ namespace Login
             string user = "TX";
             string pass = "1234";
 
-            if (textUser.Text == string.Empty || textPass.Text == string.Empty)
+            if (txtUser.Text == string.Empty || txtPass.Text == string.Empty)
             {
                 MessageBox.Show("Preencha os campos para continuar", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
 
-            if (textUser.Text == user && textPass.Text == pass)
+            if (txtUser.Text == user && txtPass.Text == pass)
             {
                 MessageBox.Show("Credenciais válidas!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                tentativas = 0;
             }
             else
             {
+                tentativas++;
+                int tentativasRestantes = 3 - tentativas;
+
+            if (tentativas >= 3)
+            {
+                    MessageBox.Show("Número máximo de tentativas excedido. Acesso bloqueado.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    btnEntrar.Enabled = false;
+                    txtPass.Enabled = false;
+                    txtUser.Enabled = false;
+            }
                 MessageBox.Show("Credenciais inválidas!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             
